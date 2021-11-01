c03c2739512200351c81840d6b82b42fea9b6e1d
Taiki Harada <haradataiki19990927@gmail.com>
2021-11-01 11:30:28 +0900

    Add examples of filtering.

---
+++ examples/cauthy_filter.py
@@ -0,0 +1,21 @@
+from typing import Tuple
+
+from skimage import io
+from matplotlib import pyplot as plt
+
+from fdimg import fft, filtering, edit
+
+img = io.imread('./image/lenna.bmp')
+height, width, color = img.shape
+
+img_fft = fft.fft2(img)
+
+meshgrid = filtering.meshgrid(size=(height, width))
+H = filtering.cauthy_psf_meshgrid(meshgrid, 4)
+
+res_fft = fft.filter2(img_fft, H)
+img_ifft = fft.ifft2(res_fft)
+
+plt.imshow(edit.convert_to_uint8(img_ifft))
+
+plt.show()
---
+++ examples/translate_filter.py
@@ -0,0 +1,18 @@
+from skimage import io
+from matplotlib import pyplot as plt
+
+from fdimg import fft, edit, filtering
+
+img = io.imread('./image/lenna.bmp')
+height, width, color = img.shape
+
+img_fft = fft.fft2(img)
+
+meshgrid = filtering.meshgrid(size=(height, width))
+H = filtering.translate_meshgrid(meshgrid, dx=100, dy=50)
+
+res_fft = fft.filter2(img_fft, H)
+img_ifft = fft.ifft2(res_fft)
+
+plt.imshow(edit.convert_to_uint8(img_ifft))
+plt.show()
